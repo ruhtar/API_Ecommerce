08fa55d112e0d686c4a56a1b39be7e0286e15452
ruhtar <arthur.amorim10@gmail.com>
2022-10-13 15:14:26 -0300

    Correção de acesso a rota de pedidos

--- routes/pedidos.js
+++ routes/pedidos.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const PedidosController = require("../controllers/PedidosController.js");
+const login = require("../middleware/login");
 
 router.get("/", PedidosController.retornaTodosPedidos);
 router.get("/:id_pedido", PedidosController.retornaUmPedido);
-router.post("/", PedidosController.criaUmPedido);
-router.delete("/", PedidosController.deletaUmPedido);
+router.post("/:id_pedido", login, PedidosController.criaUmPedido);
+router.delete("/:id_pedido", login, PedidosController.deletaUmPedido);
 
 module.exports = router;
